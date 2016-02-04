c474d00854c1b6e4b6e1a18ffdbaf99dcd7b4371
Adrian Albin-Clark <a.albin-clark@lancaster.ac.uk>
2016-02-04 11:29:58 +0000

    Fix duplicates when related datasets present in Pure metadata

--- lib/crosswalk_pure_to_datacite.rb
+++ lib/crosswalk_pure_to_datacite.rb
@@ -20,7 +20,7 @@ module CrosswalkPureToDatacite
       ) {
         xml.identifier doi, :identifierType => 'DOI'
         xml.creators {
-          creator_path = "//" + pure_dataset_response_type +
+          creator_path = pure_dataset_xpath_prefix + pure_dataset_response_type + ":persons/" + pure_dataset_response_type +
               ":dataSetPersonAssociation[person-template:personRole/core:term/core:localizedString='Creator']"
           doc.xpath(creator_path, ns).each do |creator|
             xml.creator {
@@ -40,7 +40,7 @@ module CrosswalkPureToDatacite
         }
 
         # contributors (non creators)
-        non_creator_path = "//" + pure_dataset_response_type +
+        non_creator_path =  pure_dataset_xpath_prefix + pure_dataset_response_type + ":persons/" + pure_dataset_response_type +
             ":dataSetPersonAssociation[person-template:personRole/core:term/core:localizedString!='Creator']"
         non_creator_contributor_types = doc.xpath(non_creator_path, ns)
         if !non_creator_contributor_types.empty?
@@ -61,7 +61,7 @@ module CrosswalkPureToDatacite
           }
           xml.contributors {
             contributorTypes.each do |contributorTypePure, contributorTypeDataCite|
-              contributor_path = "//" + pure_dataset_response_type + ":dataSetPersonAssociation[person-template:personRole/core:term/core:localizedString='"+contributorTypePure+"']"
+              contributor_path = pure_dataset_xpath_prefix + pure_dataset_response_type + ":dataSetPersonAssociation[person-template:personRole/core:term/core:localizedString='"+contributorTypePure+"']"
               doc.xpath(contributor_path, ns).each do |contributor|
                 xml.contributor(:contributorType => contributorTypeDataCite) {
                   xml.contributorName contributor.xpath("person-template:name/core:lastName", ns).text + ', ' + contributor.xpath("person-template:name/core:firstName", ns).text
@@ -82,9 +82,9 @@ module CrosswalkPureToDatacite
         end
 
         xml.titles {
-          xml.title doc.xpath("//" + pure_dataset_response_type + ":title/core:localizedString", ns).text
+          xml.title doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":title/core:localizedString", ns).text
         }
-        description_path = "//" + pure_dataset_response_type + ":descriptions//extensions-core:value/core:localizedString"
+        description_path = pure_dataset_xpath_prefix + pure_dataset_response_type + ":descriptions//extensions-core:value/core:localizedString"
         if !doc.xpath(description_path, ns).empty?
           xml.descriptions {
             xml.description doc.xpath(description_path, ns).text, :descriptionType => 'Abstract'
@@ -95,12 +95,12 @@ module CrosswalkPureToDatacite
           }
         end
 
-        xml.publisher doc.xpath("//" + pure_dataset_response_type + ":publisher//publisher-template:name", ns).text
+        xml.publisher doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":publisher//publisher-template:name", ns).text
 
-        t = Time.parse(doc.xpath("//core:content/core:created", ns).text)
+        t = Time.parse(doc.xpath(pure_dataset_xpath_prefix + "core:created", ns).text)
         xml.publicationYear t.strftime("%Y")
 
-        keyword_group_path = "//core:content/core:keywordGroups/core:keywordGroup/core:keyword/core:userDefinedKeyword/core:freeKeyword"
+        keyword_group_path = pure_dataset_xpath_prefix + "core:keywordGroups/core:keywordGroup/core:keyword/core:userDefinedKeyword/core:freeKeyword"
         if !doc.xpath(keyword_group_path, ns).empty?
           xml.subjects {
             doc.xpath(keyword_group_path, ns).each do |keyword_group|
@@ -114,13 +114,13 @@ module CrosswalkPureToDatacite
 
         xml.resourceType 'Dataset', :resourceTypeGeneral => 'Dataset'
         xml.alternateIdentifiers {
-          xml.alternateIdentifier doc.xpath("//core:content/@uuid", ns).text, :alternateIdentifierType => 'Pure UUID'
+          xml.alternateIdentifier doc.xpath(pure_dataset_xpath_prefix + "@uuid", ns).text, :alternateIdentifierType => 'Pure UUID'
         }
         xml.dates {
           # available
-          year = doc.xpath("//" + pure_dataset_response_type + ":dateMadeAvailable/core:year", ns).text
-          month = doc.xpath("//" + pure_dataset_response_type + ":dateMadeAvailable/core:month", ns).text
-          day = doc.xpath("//" + pure_dataset_response_type + ":dateMadeAvailable/core:day", ns).text
+          year = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":dateMadeAvailable/core:year", ns).text
+          month = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":dateMadeAvailable/core:month", ns).text
+          day = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":dateMadeAvailable/core:day", ns).text
           ymd = ''
           if !year.empty?
             ymd << year
@@ -149,11 +149,11 @@ module CrosswalkPureToDatacite
           end
         }
 
-        locale = doc.xpath("//" + pure_dataset_response_type + ":title/core:localizedString/@locale", ns).text
+        locale = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":title/core:localizedString/@locale", ns).text
         locale = locale.gsub('_', '-').downcase
         xml.language locale
 
-        geoLocationPlace = doc.xpath("//" + pure_dataset_response_type + ":geographicalCoverage/core:localizedString", ns).text
+        geoLocationPlace = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":geographicalCoverage/core:localizedString", ns).text
         # will need to test for numerical geolocations when implemented in order to create a <geoLocations> element
         if geoLocationPlace.length > 0
           xml.geoLocations {
@@ -164,7 +164,7 @@ module CrosswalkPureToDatacite
         end
 
         # sizes
-        sizes = doc.xpath("//" + pure_dataset_response_type + ":documents//core:size", ns)
+        sizes = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":documents//core:size", ns)
         if !sizes.empty?
           xml.sizes {
             sizes.each do |size|
@@ -174,7 +174,7 @@ module CrosswalkPureToDatacite
         end
 
         # formats
-        formats = doc.xpath("//" + pure_dataset_response_type + ":documents//core:mimeType", ns)
+        formats = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":documents//core:mimeType", ns)
         if !formats.empty?
           xml.formats {
             formats.each do |format|
@@ -184,7 +184,7 @@ module CrosswalkPureToDatacite
         end
 
         # licenses (current xml response only)
-        licenses = doc.xpath("//" + pure_dataset_response_type + ":documents//" + pure_dataset_response_type + ":documentLicense", ns)
+        licenses = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":documents//" + pure_dataset_response_type + ":documentLicense", ns)
         if !licenses.empty?
           xml.rightsList {
             licenses.each do |license|
@@ -198,7 +198,7 @@ module CrosswalkPureToDatacite
         includeRelatedContent = true
         if includeRelatedContent === true
           # related content (current xml response only)
-          relatedPublicationUUIDs = doc.xpath("//" + pure_dataset_response_type + ":relatedPublications/core:relatedContent/@uuid", ns)
+          relatedPublicationUUIDs = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":relatedPublications/core:relatedContent/@uuid", ns)
 
           # AS AT 2016-01-14 THERE IS NO SEMANTICALLY MEANINGFUL WAY TO INCLUDE THE RELATED PROJECT(S) IN THE METADATA
           # isDocumentedBy for a project url (stab1:projectURL) is the closest but inaccurate as it describes the project not the dataset
@@ -257,9 +257,9 @@ module CrosswalkPureToDatacite
 
     pure_dataset_response_type = get_pure_dataset_response_type(doc)
 
-    startYear = doc.xpath("//" + pure_dataset_response_type + ":temporalCoverageStartDate/core:year", ns).text
-    startMonth = doc.xpath("//" + pure_dataset_response_type + ":temporalCoverageStartDate/core:month", ns).text
-    startDay = doc.xpath("//" + pure_dataset_response_type + ":temporalCoverageStartDate/core:day", ns).text
+    startYear = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":temporalCoverageStartDate/core:year", ns).text
+    startMonth = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":temporalCoverageStartDate/core:month", ns).text
+    startDay = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":temporalCoverageStartDate/core:day", ns).text
     startDate = ''
     if !startYear.empty?
       startDate << startYear
@@ -279,9 +279,9 @@ module CrosswalkPureToDatacite
       startDate << '-' + startDay
     end
 
-    endYear = doc.xpath("//" + pure_dataset_response_type + ":temporalCoverageEndDate/core:year", ns).text
-    endMonth = doc.xpath("//" + pure_dataset_response_type + ":temporalCoverageEndDate/core:month", ns).text
-    endDay = doc.xpath("//" + pure_dataset_response_type + ":temporalCoverageEndDate/core:day", ns).text
+    endYear = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":temporalCoverageEndDate/core:year", ns).text
+    endMonth = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":temporalCoverageEndDate/core:month", ns).text
+    endDay = doc.xpath(pure_dataset_xpath_prefix + pure_dataset_response_type + ":temporalCoverageEndDate/core:day", ns).text
     endDate = ''
     if !endYear.empty?
       endDate << endYear
@@ -312,7 +312,11 @@ module CrosswalkPureToDatacite
   def get_pure_dataset_response_type(doc)
     ns = doc.collect_namespaces
     # get cur or stab before the colon
-    return doc.xpath("//core:result/core:content/@xsi:type", ns).text.split(":")[0]
+    return doc.xpath(pure_dataset_xpath_prefix + "@xsi:type", ns).text.split(":")[0]
+  end
+
+  def pure_dataset_xpath_prefix
+    "dataset:GetDataSetsResponse/core:result/core:content/"
   end
 
   def crosswalk_pure_local_to_datacite_dataset_metadata(doi, pure_dataset_metadata)
